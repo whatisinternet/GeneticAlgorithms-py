98d8bf48202e17f7e8fed2bc048dd07ca389a866
smit5260 <SJS@SJS-OE.local>
2015-11-22 16:03:39 -0500

    A start - Tested!
    All tested already:
    -called package folder 'package'
    -started main function, has a list printing function
    -Numerical simple OF function
    -2 reproduction functions: create initial random pool and reproduce
    (reproduction operator)
    -left printing to demo: run main module
    -empty modules for other operators and operators' supporting functions
    -lots of comments
    -no classes yet

---
+++ package/crossover.py
@@ -0,0 +1,6 @@
+'''
+Created on Nov 18, 2015
+
+@authors: Scott Smith, Josh Teeter, Steven Gao, Irfan Lakha
+'''
+
---
+++ package/mutation.py
@@ -0,0 +1,8 @@
+'''
+Created on Nov 18, 2015
+
+@authors: Scott Smith, Josh Teeter, Steven Gao, Irfan Lakha
+'''
+
+
+
