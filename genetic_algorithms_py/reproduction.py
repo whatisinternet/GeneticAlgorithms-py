3fe87b4246f1c5bc6e4342bb9b5f3e4f3d483988
Whatisinternet <joshteeter@gmail.com>
2015-11-22 20:18:55 -0500

    Fixed up formatting

--- genetic_algorithms_py/reproduction.py
+++ genetic_algorithms_py/reproduction.py
@@ -1,34 +1,32 @@
 '''
-Created on Nov 18, 2015
-
 @authors: Scott Smith, Josh Teeter, Steven Gao, Irfan Lakha
 
 '''
 import random
 
-        
+
 def createRandomPool( poolSize, strLength):
     '''
     Creates a random pool of strings of binary digits for specified string length and pool size
     '''
-  
+
     poolList = [ ]
-    
-  
+
+
     for i in range(0,poolSize):       #traverse all bit strings in list
         curString = ''                  #init next new string
-        
-        
+
+
         for j in range(0, strLength): #traverse all bits in a string
-            
+
             randBit = random.randint(0,1)
-    
+
             curString+= str(randBit)         #append random bit
-    
+
         poolList.append(curString)         #random string complete, add to list
 
 
-    return poolList 
+    return poolList
 
 
 
@@ -37,25 +35,25 @@ def reproduce( objectiveFunction, PoolListIn, poolOutSize, targetBitStr):
     Inputs a list of bit strings (input pool) and uses the objective function (OF)
     to produce a new pool. The OF outputs a list of probabilities that the string should
     appear in the output pool (size can be chosen). The string is either chosen
-    or not in its entirety 0 or multiple times, chosen by a weighted randomized selection 
+    or not in its entirety 0 or multiple times, chosen by a weighted randomized selection
     (ie. probability based choice).
-    
-    objectiveFunction must have parameters(List PoolListIn, String targetBitStr)  
+
+    objectiveFunction must have parameters(List PoolListIn, String targetBitStr)
 
     '''
     poolOut = [ ]
 
     poolInSize = len(PoolListIn)      #same size as OFResultList  (pool values correspond to OF results)
-  
+
     print('pool size: '+str(poolInSize))
     #OF outputs a list of percentages corresponding to probabitlity that string should be chosen
-    OFResultList = objectiveFunction(PoolListIn, targetBitStr) 
-    
+    OFResultList = objectiveFunction(PoolListIn, targetBitStr)
+
     print('')
     print('OF listlength: ')
     print( len(OFResultList) )
     print('OF list:')
-    printList(OFResultList) 
+    printList(OFResultList)
     print('')
 
     #percentages are given as values between 0 and 1
@@ -63,16 +61,16 @@ def reproduce( objectiveFunction, PoolListIn, poolOutSize, targetBitStr):
 
     #search for choice (weighted random selection - probability)
     for i in range(0, poolOutSize):
-        
-        
+
+
         found = False
-        
-        
-        
+
+
+
         while not found:
         #choice may still lie between added percentages and max percent, produce a new choice if in this range
         # (ie. if probability is %99, this is the %1 that nothing is found)
-        
+
             choice = random.uniform(0,maxPercentage)        #choose percentage between 0 and added values
             currentTotal = 0       #init/reset
 
@@ -80,16 +78,16 @@ def reproduce( objectiveFunction, PoolListIn, poolOutSize, targetBitStr):
             for j in range(0,poolInSize):
 
                 if choice >= currentTotal and choice <= currentTotal + OFResultList[j]:
-                
+
                     stringIndexChosen = j
                     found = True
                     break       #found, end search (current loop)
 
                 currentTotal += OFResultList[j]      #add actual percentage values to create weight distribution
 
-        
+
         #found, record and choose next string for output list
-        print('string chosen: ' + PoolListIn[stringIndexChosen] + ', chose #: ' + str(stringIndexChosen+1) +', time:' +str(i+1)) 
+        print('string chosen: ' + PoolListIn[stringIndexChosen] + ', chose #: ' + str(stringIndexChosen+1) +', time:' +str(i+1))
         poolOut.append(PoolListIn[stringIndexChosen])
 
 
@@ -104,5 +102,5 @@ def printList(list1):
 
     for i in range(0, size):
         print(list1[i])
-     
+
 
