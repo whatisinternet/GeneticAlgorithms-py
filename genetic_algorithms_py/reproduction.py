1d0ac0e92ffc6a3734061d1558c75510af275eb2
Whatisinternet <joshteeter@gmail.com>
2015-11-27 00:51:41 -0500

    Getting ready for reproduction. Sorting, weighting, all ready.

--- genetic_algorithms_py/reproduction.py
+++ genetic_algorithms_py/reproduction.py
@@ -1,7 +1,19 @@
+import seeding
+
 def reproduce( objective_function, current_pool, target):
-    return _build_dictionary( objective_function, current_pool )
+    sorted_pool = _sort_dictionary(_build_dictionary( objective_function, current_pool ))
+    total_fitness = _total_fitness(sorted_pool)
+    return current_pool
 
 
+# private ---
 def _build_dictionary( objective_function, current_pool ):
     return list(map((lambda x: {'seed': x, 'weight': objective_function(x)}),
         current_pool))
+
+def _sort_dictionary( dictionary ):
+    return sorted(dictionary, key=lambda x: x['weight'], reverse=True)
+
+def _total_fitness( dictionary ):
+    weights = list(map((lambda x: x['weight']), dictionary))
+    return reduce((lambda x, y: x+y), weights)
--- tests/reproduction_tests.py
+++ tests/reproduction_tests.py
@@ -1,8 +1,56 @@
 from nose.tools import *
+import itertools
 from genetic_algorithms_py import seeding
 from genetic_algorithms_py import reproduction
 
-def test_it_should_return_an_array_of_string():
+def test_it_should_return_an_array_of_size_8():
     seeding_pool = seeding.pool(8)
     asserted_pool = reproduction.reproduce((lambda x: int(x, 2) ** 2), seeding_pool, '11111111')
     assert len(asserted_pool) == 8
+
+def test_it_should_return_an_array_of_strings():
+    seeding_pool = seeding.pool(8)
+    asserted_pool = reproduction.reproduce((lambda x: int(x, 2) ** 2), seeding_pool, '11111111')
+    for asserted in asserted_pool:
+        assert isinstance( asserted, str )
+
+# Testing private methods NB: Wouldn't normally keep these
+
+def test__build_dictionary_should_return_a_collection_of_size_8():
+    seeding_pool = seeding.pool(8)
+    asserted_dictionary = reproduction._build_dictionary((lambda x: int(x, 2) ** 2), seeding_pool)
+    assert len(asserted_dictionary) == 8
+
+def test__build_dictionary_should_return_a_collection_of_dictionaries():
+    seeding_pool = seeding.pool(8)
+    asserted_dictionary= reproduction._build_dictionary((lambda x: int(x, 2) ** 2), seeding_pool)
+    for asserted in asserted_dictionary:
+        assert isinstance(asserted, dict)
+
+def test__build_dictionary_should_return_a_collection_of_dictionaries_with_key_seed():
+    seeding_pool = seeding.pool(8)
+    asserted_dictionary= reproduction._build_dictionary((lambda x: int(x, 2) ** 2), seeding_pool)
+    for asserted in asserted_dictionary:
+        assert isinstance(asserted['seed'], str)
+
+def test__build_dictionary_should_return_a_collection_of_dictionaries_with_key_weight():
+    seeding_pool = seeding.pool(8)
+    asserted_dictionary= reproduction._build_dictionary((lambda x: int(x, 2) ** 2), seeding_pool)
+    for asserted in asserted_dictionary:
+        assert isinstance(asserted['weight'], int)
+
+def test__sort_dictionary_should_sort_the_collection_by_weight_decending():
+    seeding_pool = seeding.pool(8)
+    dictionary= reproduction._build_dictionary((lambda x: int(x, 2) ** 2), seeding_pool)
+    asserted_dictionary = reproduction._sort_dictionary(dictionary)
+    asserted_values = reversed(list(map((lambda x: x['weight']), asserted_dictionary)))
+    it = iter(asserted_values)
+    it.next()
+    is_sorted = all(b >= a for a, b in itertools.izip(asserted_values, it))
+    assert is_sorted == True
+
+def test__total_fitness():
+    seeding_pool = seeding.pool(8)
+    dictionary = reproduction._build_dictionary((lambda x: int(x, 2) ** 2), seeding_pool)
+    asserted_total = reproduction._total_fitness(dictionary)
+    assert isinstance(asserted_total, int)
