354f9d14353fa8a9f510b6029c8dfaf293af8d0d
Whatisinternet <joshteeter@gmail.com>
2015-11-27 03:18:54 -0500

    Added main function and base tests for the main function. All passing

--- genetic_algorithms_py/__init__.py
+++ genetic_algorithms_py/__init__.py
@@ -1,5 +1,19 @@
-import random_vector
+import mutation
+import seeding
+import reproduction
+import crossover
+def __init__(black_box, iterations):
+    seeding_pool = seeding.pool(8)
+    final_pool = _aux(seeding_pool, black_box, 0, iterations)
+    print seeding_pool
+    print final_pool
+    return final_pool
 
-def __init__():
-    initial_vector = random_vector.initialization_vector()
-    return initial_vector
+def _aux(seed, black_box, depth, max_iterations):
+    if depth == max_iterations:
+        return seed
+    else:
+        pool = reproduction.reproduce(black_box, seed)
+        crossed_over = crossover.crossover(pool)
+        mutated = mutation.mutate_pool(crossed_over)
+        return _aux(mutated, black_box, depth + 1, max_iterations)
