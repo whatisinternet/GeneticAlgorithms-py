060737ee08309a0cb5746478dd4906d6bdaaa13c
whatisinternet <joshteeter@gmail.com>
2015-11-28 14:05:50 -0500

    Fixed up final iteration to actually fully run

--- genetic_algorithms_py/__init__.py
+++ genetic_algorithms_py/__init__.py
@@ -2,6 +2,7 @@ import mutation
 import seeding
 import reproduction
 import crossover
+
 def __init__(black_box, iterations, bit_size):
     seeding_pool = seeding.pool(8, bit_size)
     final_pool = _aux(seeding_pool, black_box, 0, iterations)
@@ -9,15 +10,17 @@ def __init__(black_box, iterations, bit_size):
     print final_pool
     return final_pool
 
+
 def _aux(seed, black_box, depth, max_iterations):
     print '\n', depth, 'Seed       ', seed
+    pool = reproduction.reproduce(black_box, seed)
+    print ' reproduction ',  pool
+    crossed_over = crossover.crossover(pool)
+    print ' crossover    ',  crossed_over
+    mutated = mutation.mutate_pool(crossed_over)
+    print ' mutated      ',  mutated
+
     if depth == max_iterations:
         return seed
     else:
-        pool = reproduction.reproduce(black_box, seed)
-        print ' reproduction ',  pool
-        crossed_over = crossover.crossover(pool)
-        print ' crossover    ',  crossed_over
-        mutated = mutation.mutate_pool(crossed_over)
-        print ' mutated      ',  mutated
         return _aux(mutated, black_box, depth + 1, max_iterations)
