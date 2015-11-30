d3f6e239798d6cd90f836b31ddbb042b199f0816
Whatisinternet <joshteeter@gmail.com>
2015-11-29 23:36:56 -0500

    Refactored output to be more csv like

--- example.py
+++ example.py
@@ -16,36 +16,28 @@ target = "10101010" # MUST be set and equal to bit size
 debug = _is_debugging()
 
 def x_2():
-    print '-------------------------'
-    print 'Blackbox: x^2'
+    print 'Blackbox: x^2 function target={target} \n'.format(target=target)
     black_box = (lambda x: int(x, 2) ** 2)
     genetic_algorithms_py.__init__(black_box, iterations, bit_size, target, "X^2")
-    print 'Blackbox: deJongSphere', target
 
 def dejong():
-    print '-------------------------'
-    print 'Blackbox: deJongSphere'
+    print 'Blackbox: deJongSphere function target={target} \n'.format(target=target)
     black_box = (lambda x: int(x, 2) + int(target, 2) ** 2)
     genetic_algorithms_py.__init__(black_box, iterations, bit_size, target, "deJong Sphere")
-    print 'Blackbox: deJongSphere', target
 
 def rosenbrock():
-    print '-------------------------'
-    print 'Blackbox: Rosenbrock function'
+    print 'Blackbox: Rosenbrock function target={target} \n'.format(target=target)
     a = random.randrange(0, 100)
     b = random.randrange(a, 100)
     black_box = (lambda x: (a + int(x, 2)) ** 2 + b * (
         int(target, 2) - int(x, 2) ** 2) ** 2)
     genetic_algorithms_py.__init__(black_box, iterations, bit_size, target, "Rosenbrock Function")
-    print 'Blackbox: Rosenbrock function', target
 
 def himmelblau():
-    print '-------------------------'
-    print 'Blackbox: Himmelblau function'
+    print 'Blackbox: Himmelblau function target={target} \n'.format(target=target)
     black_box = (lambda x: (((int(x, 2) ** 2) + int(target, 2) - 11) ** 2) +
             ((int(x, 2) + (int(target, 2) ** 2) - 7) ** 2))
     genetic_algorithms_py.__init__(black_box, iterations, bit_size, target, "Himmelblau Function")
-    print 'Blackbox: Himmelblau function', target
 
 x_2()
 if debug:
--- genetic_algorithms_py/__init__.py
+++ genetic_algorithms_py/__init__.py
@@ -14,26 +14,26 @@ def __init__(black_box, iterations, bit_size,
 
     seeding_pool = seeding.pool(8, bit_size)
     final_pool, state = _aux(seeding_pool, black_box, 0, iterations, target)
-    print seeding_pool
-    print final_pool, state
     if _is_debugging():
+        print seeding_pool
+        print final_pool, state
         _debug_chart(target, function_name)
     return final_pool
 
 
 def _aux(seed, black_box, depth, max_iterations, target):
-    print '\n', depth, 'Seed       ', seed
+    _format_output(['{a} Seed'.format(a=depth)] + seed)
     if depth == max_iterations:
         return seed, "NOT FOUND!"
     elif target in seed:
         return seed, "FOUND!"
     else:
         pool = reproduction.reproduce(black_box, seed)
-        print ' reproduction ',  pool
+        _format_output(['reproduction'] + pool)
         crossed_over = crossover.crossover(pool)
-        print ' crossover    ',  crossed_over
+        _format_output(['crossover'] + crossed_over)
         mutated = mutation.mutate_pool(crossed_over)
-        print ' mutated      ',  mutated
+        _format_output(['mutated'] + mutated)
         return _aux(mutated, black_box, depth + 1, max_iterations, target)
 
 def _debug_chart(target, function_name):
@@ -53,3 +53,6 @@ def _remove_fitness_data():
             os.remove('./fitness_data.csv')
         except OSError:
             pass
+
+def _format_output(collection):
+    print ",".join(collection)
