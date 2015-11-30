f5d87bf1d72601b95d0c2d6fd0676249f0b8b209
whatisinternet <joshteeter@gmail.com>
2015-11-30 18:30:07 -0500

    Fixed up init to meet PEP0008

--- genetic_algorithms_py/__init__.py
+++ genetic_algorithms_py/__init__.py
@@ -5,12 +5,13 @@ import crossover
 import plotter
 import os
 
-def __init__(black_box, iterations, bit_size, pool_size = 8,
-        target = None, function_name = None):
+
+def __init__(black_box, iterations, bit_size,
+             pool_size=8, target=None, function_name=None):
     _remove_fitness_data()
 
-    if target == None:
-        target = reduce( lambda x, y: x + y ** 2, range(bit_size))
+    if target is None:
+        target = reduce(lambda x, y: x + y ** 2, range(bit_size))
 
     seeding_pool = seeding.pool(pool_size, bit_size)
     final_pool, state = _aux(seeding_pool, black_box, 0, iterations, target)
@@ -36,16 +37,19 @@ def _aux(seed, black_box, depth, max_iterations, target):
         _format_output(['mutated'] + mutated)
         return _aux(mutated, black_box, depth + 1, max_iterations, target)
 
+
 def _debug_chart(target, function_name):
     plotter.chart(target, function_name)
 
+
 def _is_debugging():
-    f = open('./debug','r')
+    f = open('./debug', 'r')
     debug = f.readline()
-    if debug  == "True\n":
+    if debug == "True\n":
         return True
     else:
         return False
+
 
 def _remove_fitness_data():
     if _is_debugging():
@@ -53,6 +57,7 @@ def _remove_fitness_data():
             os.remove('./fitness_data.csv')
         except OSError:
             pass
+
 
 def _format_output(collection):
     print ",".join(collection)
