f5e76262a4bab34a29b106e553d3e28b754a9328
Whatisinternet <joshteeter@gmail.com>
2015-12-01 20:05:51 -0500

    Fixed mutation tests to pass the correct number of parameters

--- tests/mutation_tests.py
+++ tests/mutation_tests.py
@@ -4,9 +4,11 @@ from genetic_algorithms_py import reproduction
 from genetic_algorithms_py import crossover
 
 black_box = (lambda x, y: int(y) + int(x) ** 2)
+mutation_probability = 0.5
+
 
 def test_it_should_return_a_mutated_string():
-    mutated_string = mutation.mutate("101")
+    mutated_string = mutation.mutate("101", mutation_probability)
     assert len(mutated_string) == 3
 
 
@@ -14,7 +16,7 @@ def test_it_should_mutate_a_pool_returning_2_strings():
     seeding_pool = seeding.pool(2, 8)
     pool = reproduction.reproduce(black_box, seeding_pool, 2)
     crossed_over = crossover.crossover(pool)
-    asserted_pool = mutation.mutate_pool(crossed_over)
+    asserted_pool = mutation.mutate_pool(crossed_over, mutation_probability)
     assert len(asserted_pool) == 2
 
 
@@ -22,6 +24,6 @@ def test_it_should_mutate_a_pool_returning_n_strings():
     seeding_pool = seeding.pool(2, 8)
     pool = reproduction.reproduce(black_box, seeding_pool, 2)
     crossed_over = crossover.crossover(pool)
-    asserted_pool = mutation.mutate_pool(crossed_over)
+    asserted_pool = mutation.mutate_pool(crossed_over, mutation_probability)
     for asserted in asserted_pool:
         assert isinstance(asserted, str)
