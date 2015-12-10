858bbcd55e94ba7b1cc18510039fc70f4043d083
Whatisinternet <joshteeter@gmail.com>
2015-12-09 23:12:20 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='genetic_algorithms_py',
         version='0.0.1',
-        description='Implements the SGA as outlined in the final project for WLU CP468',
+        description='Implements the a simple genetic algorithm',
         url='http://github.com/whatisinternet/GeneticAlgorithms-py',
         author='Josh Teeter, Steven Gao, Irfan Lakha, Scott Smith',
         author_email='joshteeter@gmail.com',
